808bcfa71ae3275f0c8ce964d7a2c2efa959f3f3
nTraum <philipp.press@googlemail.com>
2012-11-13 15:05:05 +0100

    released 1.0

--- etf2l_admin.py
+++ etf2l_admin.py
@@ -6,25 +6,51 @@ import exceptions
 
 __module_name__ = 'ETF2L Admin'
 __module_version__ = '1.0'
-__module_description__ = '''
-Easy admin-request handling for ETF2L admins.
+__module_description__ = '''Easy admin-request handling for ETF2L admins.
 When the bot announces a request, type `/g` to take care of it. The user will automatically be queried for you.
 '''
 
+HELP_MSG='''/r <ID> takes request with specified ID.
+/r takes latest request.'''
 ADMIN_CHANNEL = '#etf2l.admins'
-ADMIN_CHANNEL = '#ntraum'
 REGEX_BOT_REQUEST_MSG = r'Info: Admin requested in (?P<chan>#.+) by (?P<nick>.+), Request ID: (?P<id>\d+)'
 DEBUG = False
-DEBUG = True
 
-requests = {}
-latest_id = None
+class RequestHandler:
+  def __init__(self):
+    self.requests = {}
+    self.latest_id = None
+
+  def handle_latest_request(self, context):
+    if None == self.latest_id:
+      say('Latest request unknown.')
+      return xchat.EAT_XCHAT
+    return self.handle_request(self.latest_id, context)
+
+  def handle_request(self, id, context):
+    if id in self.requests:
+      say('Taking request #{}'.format(id))
+      nick = self.requests.pop(id)
+      context.command('say !got {}'.format(id))
+      context.command('query {}'.format(nick))
+      context.command('msg {} Hi {}, how can I help you?'.format(nick, nick))
+    else:
+      say('Unknown ID: #{}'.format(id))
+    return xchat.EAT_XCHAT
+
+  def add_request(self, id, nick):
+    self.latest_id = id
+    self.requests[id] = nick
+
+
+def say(msg):
+  print('[{}] {}'.format(__module_name__, msg))
 
 def debug(msg):
   if DEBUG:
-    print('[{}] {}'.format(__module_name__, msg))
+    say(msg)
 
-def on_msg(word, word_eol, userdata):
+def on_msg(word, word_eol, handler):
   msg = word[1]
   channel = xchat.get_context().get_info('channel')
   debug('Message: {}'.format(msg))
@@ -38,9 +64,8 @@ def on_msg(word, word_eol, userdata):
     try:
       curr_user = m.group('nick')
       curr_id = int(m.group('id'))
-      latest_id = curr_id
-      requests[curr_id] = curr_user
-      debug('Request added, Nick: {}, ID: {}'.format(curr_user, curr_id))
+      h.add_request(curr_id, curr_user)
+      debug('Request added, Nick: {}, ID: #{}'.format(curr_user, curr_id))
       return xchat.EAT_NONE
     except exceptions.ValueError:
       debug('Could not parse {} to integer'.format(m.group('id')))
@@ -49,26 +74,32 @@ def on_msg(word, word_eol, userdata):
     debug('Message did not match regex, ignoring...')
     return xchat.EAT_NONE
 
-def r(word, word_eol, userdata):
+def r(word, word_eol, handler):
   channel = xchat.get_context().get_info('channel')
-  if 1 == len(word):
-    debug('No ID specified')
+  if channel != ADMIN_CHANNEL:
+    say('This command can be used in {} only.'.format(ADMIN_CHANNEL))
     return xchat.EAT_XCHAT
+  if len(word) > 2:
+    say('Too many arguments, type \'/help r\' to get help.')
+    return xchat.EAT_XCHAT
+
+  if 1 == len(word):
+    debug('No ID specified.')
+    return h.handle_latest_request(xchat.get_context())
   elif 2 == len(word):
     try:
       id = int(word[1])
-      debug('ID specified: {}'.format(id))
-      return xchat.EAT_XCHAT
+      debug('ID specified: #{}.'.format(id))
+      return h.handle_request(id, xchat.get_context())
     except exceptions.ValueError:
-      debug('Could not parse {} to integer'.format(word[1]))
+      say('\'{}\' is no valid ID.'.format(word[1]))
       return xchat.EAT_XCHAT
-  else:
-    debug('Too many arguments, ignoring...')
-    return xchat.EAT_XCHAT
 
-xchat.prnt('{} version {} loaded.'.format(__module_name__, __module_version__))
+say('{} version {} loaded.'.format(__module_name__, __module_version__))
 debug('Admin channel: {}'.format(ADMIN_CHANNEL))
 debug('Regex: {}'.format(REGEX_BOT_REQUEST_MSG))
 
-xchat.hook_print('Channel Message', on_msg)
-xchat.hook_command('r', r)
+h = RequestHandler()
+xchat.hook_print('Channel Message', on_msg, h)
+xchat.hook_print('Channel Msg Hilight', on_msg, h)
+xchat.hook_command('r', r, h,help=HELP_MSG)
