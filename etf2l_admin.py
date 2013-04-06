89e8e2bdd95ad43a9a846ea6d5ba899450227203
nTraum <philipp.press@googlemail.com>
2013-04-07 01:41:48 +0200

    fixed regex, plugin actually works now. thanks emb

--- etf2l_admin.py
+++ etf2l_admin.py
@@ -5,15 +5,16 @@ import re
 import exceptions
 
 __module_name__ = 'ETF2L Admin'
-__module_version__ = '1.0'
+__module_version__ = '1.1'
 __module_description__ = '''Easy admin-request handling for ETF2L admins.
-When the bot announces a request, type `/g` to take care of it. The user will automatically be queried for you.
+When the bot announces a request, type `/r` to take care of it. The user will automatically be queried for you.
 '''
 
 HELP_MSG='''/r <ID> takes request with specified ID.
 /r takes latest request.'''
 ADMIN_CHANNEL = '#etf2l.admins'
 REGEX_BOT_REQUEST_MSG = r'Info: Admin requested in (?P<chan>#.+) by (?P<nick>.+), Request ID: (?P<id>\d+)'
+REGEX_BOT_REMINDER_MSG = r'Open admin request by (?P<nick>.+) ID: (?P<id>\d+)'
 DEBUG = False
 
 class RequestHandler:
@@ -23,24 +24,30 @@ class RequestHandler:
 
   def handle_latest_request(self, context):
     if None == self.latest_id:
-      say('Latest request unknown.')
-      return xchat.EAT_XCHAT
+      say('Request queue is empty.')
+      return xchat.EAT_XCHAT 
     return self.handle_request(self.latest_id, context)
 
   def handle_request(self, id, context):
-    if id in self.requests:
-      say('Taking request #{}'.format(id))
-      nick = self.requests.pop(id)
-      context.command('say !got {}'.format(id))
-      context.command('query {}'.format(nick))
-      context.command('msg {} Hi {}, how can I help you?'.format(nick, nick))
+   if id in self.requests:
+    say('Taking request #{}'.format(id))
+    nick = self.requests.pop(id)
+    context.command('say !got {}'.format(id))
+    context.command('query {}'.format(nick))
+    context.command('msg {} Hi {}, how can I help you?'.format(nick, nick))
+    if len(self.requests) != 0:
+     self.latest_id = self.requests.iterkeys()
     else:
-      say('Unknown ID: #{}'.format(id))
-    return xchat.EAT_XCHAT
+     debug('Request queue is now empty.')
+     self.latest_id = None
+   else:
+    say('Unknown ID: #{}'.format(id))
+   return xchat.EAT_XCHAT
 
   def add_request(self, id, nick):
-    self.latest_id = id
-    self.requests[id] = nick
+   self.latest_id = id
+   self.requests[id] = nick
+   debug('Added request to queue: #{} by {}'.format(id,nick))
 
 
 def say(msg):
@@ -51,14 +58,16 @@ def debug(msg):
     say(msg)
 
 def on_msg(word, word_eol, handler):
-  msg = word[1]
+
+  colorRe = re.compile(r"(||[0-9]{1,2}(,[0-9]{1,2}|))")
+  msg = colorRe.sub("",word[1])
   channel = xchat.get_context().get_info('channel')
   debug('Message: {}'.format(msg))
   debug('Channel: {}'.format(channel))
   if channel != ADMIN_CHANNEL:
     debug('Wrong channel, ignoring message...')
     return xchat.EAT_NONE
-
+	
   m = re.match(REGEX_BOT_REQUEST_MSG, msg)
   if m:
     try:
@@ -71,8 +80,19 @@ def on_msg(word, word_eol, handler):
       debug('Could not parse {} to integer'.format(m.group('id')))
       return xchat.EAT_NONE
   else:
-    debug('Message did not match regex, ignoring...')
-    return xchat.EAT_NONE
+   m = re.match(REGEX_BOT_REMINDER_MSG, msg)
+   if m:
+     try:
+       curr_user = m.group('nick')
+       curr_id = int(m.group('id'))
+       h.add_request(curr_id, curr_user)
+       debug('Request added, Nick: {}, ID: #{}'.format(curr_user, curr_id))
+       return xchat.EAT_NONE
+     except exceptions.ValueError:
+       debug('Could not parse {} to integer'.format(m.group('id')))
+       return xchat.EAT_NONE   
+   debug('Message did not match regex, ignoring...')
+   return xchat.EAT_NONE
 
 def r(word, word_eol, handler):
   channel = xchat.get_context().get_info('channel')
@@ -97,9 +117,10 @@ def r(word, word_eol, handler):
 
 say('{} version {} loaded.'.format(__module_name__, __module_version__))
 debug('Admin channel: {}'.format(ADMIN_CHANNEL))
-debug('Regex: {}'.format(REGEX_BOT_REQUEST_MSG))
-
+debug('Request Regex: {}'.format(REGEX_BOT_REQUEST_MSG))
+debug('Reminder Regex: {}'.format(REGEX_BOT_REMINDER_MSG))
 h = RequestHandler()
+
 xchat.hook_print('Channel Message', on_msg, h)
 xchat.hook_print('Channel Msg Hilight', on_msg, h)
 xchat.hook_command('r', r, h,help=HELP_MSG)
